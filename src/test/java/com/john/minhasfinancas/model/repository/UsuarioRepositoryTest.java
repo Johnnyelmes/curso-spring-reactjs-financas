fc8f7fb1ba68e7c335cec29ad2ba5096a6662cfa
Johnnyel <johnnyel.px5@gmail.com>
2020-05-16 18:36:42 -0300

    Alterando as annotation de test

--- src/test/java/com/john/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/john/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -2,15 +2,15 @@ package com.john.minhasfinancas.model.repository;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.john.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
