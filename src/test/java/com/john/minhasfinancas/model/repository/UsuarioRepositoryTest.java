c86e126245690cd68b7648cf7e4b99c8b4ada334
Johnnyel <johnnyel.px5@gmail.com>
2020-05-16 19:57:52 -0300

    Teste do UsuarioService no validarEmail

--- src/test/java/com/john/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/john/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.john.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 	
 	@Autowired
@@ -22,10 +24,24 @@ public class UsuarioRepositoryTest {
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
 		repository.save(usuario);
 		
-		//ação/ execcucao
+		//ação/execucao
 		boolean result = repository.existsByEmail("usuario@email.com");
 		
 		//verificacao
 		Assertions.assertTrue(result);
+	}
+	
+	
+	@Test
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
+		//cenario
+		repository.deleteAll();
+		
+		//ação
+		boolean result = repository.existsByEmail("usuario@email.com");
+		
+		//verificacao
+		Assertions.assertFalse(result);
+		
 	}
 }
---
+++ src/test/java/com/john/minhasfinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,44 @@
+package com.john.minhasfinancas.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.john.minhasfinancas.exception.RegraNegocioException;
+import com.john.minhasfinancas.model.entity.Usuario;
+import com.john.minhasfinancas.model.repository.UsuarioRepository;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class UsuarioServiceTest {
+	
+	@Autowired
+	UsuarioService service;
+	
+	@Autowired
+	UsuarioRepository repository;
+	
+	@Test
+	public void deveValidarEmail() {
+		//cenario
+		repository.deleteAll();
+		//acao
+		service.validarEmail("email@email.com");
+		
+	}
+	
+	@Test
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		//cenario
+		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
+		repository.save(usuario);
+		//acao
+		Assertions.assertThrows(RegraNegocioException.class, () -> service.validarEmail("email@email.com"));
+	}
+
+}
