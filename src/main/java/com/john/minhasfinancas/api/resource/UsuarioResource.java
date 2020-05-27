a651444dc359615430f27d1425dfbb969fe87116
Johnnyel <johnnyel.px5@gmail.com>
2020-05-27 15:11:46 -0300

    Refatorando os controllers usuario e lancamento

--- src/main/java/com/john/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/john/minhasfinancas/api/resource/UsuarioResource.java
@@ -13,15 +13,14 @@ import com.john.minhasfinancas.exception.RegraNegocioException;
 import com.john.minhasfinancas.model.entity.Usuario;
 import com.john.minhasfinancas.service.UsuarioService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioResource {
 
-	private UsuarioService service;
-
-	public UsuarioResource(UsuarioService service) {
-		this.service = service;
-	}
+	private final UsuarioService service;
 
 	@PostMapping("/autenticar")
 	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
